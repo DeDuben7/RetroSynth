5ec97fcce03bf47d29f29422bea9617006be1295
Ruben <ruben@vanrenswoude.info>
2024-06-28 20:08:19 +0200

    chore: updated template files

--- Inc/template.h
+++ Inc/template.h
@@ -1,15 +1,24 @@
 /**
-******************************************************************************
-* @file           : name.h
-* @brief          : description
-******************************************************************************
-* @attention
-*
-* <h2><center>&copy; Copyright (c) 2024 Ruben van Renswoude.
-* All rights reserved.</center></h2>
-*
-******************************************************************************
-*/
+ ******************************************************************************
+ * @file     name.h
+ * @brief    short description
+ * @version	version
+ * @author		R. van Renswoude
+ * @date		  2024
+ ******************************************************************************
+ * @details	detailed description
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2024 Ruben van Renswoude.
+ * All rights reserved.</center></h2>
+ *
+ ******************************************************************************
+ */
+/**
+ * @addtogroup	YM
+ * @{
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __NAME_H
